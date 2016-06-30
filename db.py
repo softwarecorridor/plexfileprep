23e3720ab29018021394366211ca032489bd6b57
softwarecorridor <mangadazed@gmail.com>
2016-06-30 17:41:30 -0400

    initial framework for db module

---
+++ db.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+PATH_TO_DB = ""
+
+def create_tables():
+    pass
+
+
+def add_entry(old_path, new_path):
+    pass
+
+
+def get_entry_by_newest_path(new_path):
+    pass
+
+
+def clear_db():
+    pass
+
