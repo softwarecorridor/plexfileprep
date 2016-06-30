64424d79725a1510fd7504a047215a28c978f337
softwarecorridor <mangadazed@gmail.com>
2016-06-30 18:03:35 -0400

    fill out #create_tables and #add_entry

--- db.py
+++ db.py
@@ -3,10 +3,13 @@ import sqlite3
 PATH_TO_DB = ""
 
 def create_tables():
-    pass
+    create_statement = '''CREATE TABLE {table} ({old} text, {new} text)'''
+    execute(create_statement)
 
 
 def add_entry(old_path, new_path):
+    insert_statement = '''INSERT INTO {table} VALUES (?, ?)'''
+    execute(insert_statement, [old_path, new_path])
     pass
 
 
@@ -17,3 +20,11 @@ def get_entry_by_newest_path(new_path):
 def clear_db():
     pass
 
+def execute(statement, args = []):
+    conn = sqlite3.connect(PATH_TO_DB)
+
+    c = conn.cursor()
+    c.execute(statement, args)
+
+    conn.commit()
+    conn.close()
